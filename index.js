75ff66c02180cc7fe4d89018e56c5ebe52ea3223
rmoise <roderickfmoise@gmail.com>
2022-10-07 11:28:28 +0200

    move middleware at the top of the file

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to my app!');
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.use(express.static('public'));
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
@@ -56,11 +61,6 @@ let topTenMovies = [
         title: 'Inglourious Basterds'
     }
 ];
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
