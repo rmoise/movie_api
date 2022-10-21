7740a97a2dbdc7769ae6c9a9db19f0f14dc18bbe
rmoise <roderickfmoise@gmail.com>
2022-10-21 23:00:30 +0200

    update spacing

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
 
-
-
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
