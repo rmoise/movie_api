eed3518ba057dfc08c5adec8e878a155969c426b
rmoise <roderickfmoise@gmail.com>
2022-10-21 22:14:02 +0200

    update cors

--- index.js
+++ index.js
@@ -23,21 +23,7 @@ mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                // If a specific origin isn’t found on the list of allowed origins
-                let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-                return callback(new Error(message), false);
-            }
-            return callback(null, true);
-        }
-    })
-);
+app.use(cors());
 
 // set up routes
 app.use('/auth', authRoutes);
