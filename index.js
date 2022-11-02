ae738a8da3ed5a6795bafdeeaff20a1558d553bd
rmoise <roderickfmoise@gmail.com>
2022-11-02 11:51:24 +0100

    update cors to allow react app

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 // app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'https://myflix-firstapi-app.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:8080', 'https://myflix-firstapi-app.herokuapp.com/', 'http://localhost:1234'];
 
 app.use(
     cors({
