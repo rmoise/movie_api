c886773950ed01ef2b63f686e798cbf103d7b530
rmoise <roderickfmoise@gmail.com>
2022-11-14 18:05:50 +0100

    update cors

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
 
-/* const allowedOrigins = [
+const allowedOrigins = [
     'http://localhost:8080',
     'https://api.themoviedb.org/3',
     'https://myflix-firstapi-app.herokuapp.com/',
@@ -43,13 +43,7 @@ app.use(
             return callback(null, true);
         }
     })
-); */
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // or specify your domain i.e. https://localhost.com
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+);
 
 let auth = require('./auth')(app);
 const passport = require('passport');
