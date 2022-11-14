6e519f4a546db341b542edf45387cf30cbb09e08
rmoise <roderickfmoise@gmail.com>
2022-11-14 20:51:41 +0100

    update cors

--- index.js
+++ index.js
@@ -45,12 +45,11 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     next();
 });
 
