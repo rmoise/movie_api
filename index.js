3ce528038f43b7bf6ed9fce73a7b43715a9a1fe9
rmoise <roderickfmoise@gmail.com>
2023-01-27 12:31:07 +0100

    update get user

--- index.js
+++ index.js
@@ -123,6 +123,17 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
         });
 });
 
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
 // add a user
 app.post(
     '/users',
