1d75b365cbf6ca785b6a9cb6d8013715f56fa884
rmoise <roderickfmoise@gmail.com>
2023-01-31 12:17:51 +0100

    add JSDoc documentation

--- auth.js
+++ auth.js
@@ -3,11 +3,17 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
 
-const express = require('express');
-const router = express.Router();
+/* const express = require('express'); */
+/* const router = express.Router(); */
 
 require('./passport'); // local passport file
 
+/**
+ * Creates JWT (expiring in 7 days, using HS256 algorithm to encode)
+ * @param {object} user
+ * @returns user object, jwt, and additional information on token
+ * @function generateJWTToken
+ */
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
         subject: user.Username, // This is the username your're encoding in the JWT
@@ -17,11 +23,21 @@ let generateJWTToken = (user) => {
 };
 
 // POST login.
+/**
+ * Handles user login, generating a JWT upon login
+ * Request body: A JSON object holding Username and Password.
+ * @name postLogin
+ * @kind function
+ * @param router
+ * @returns A JSON object holding the user object and JWT
+ * @requires passport
+ */
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', { session: false }, (error, user, info) => {
+        passport.authenticate('local', { session: false }, (error, user) => {
             if (error || !user) {
                 return res.status(400).json({
+                    error,
                     message: 'Something is not right',
                     user: user
                 });
